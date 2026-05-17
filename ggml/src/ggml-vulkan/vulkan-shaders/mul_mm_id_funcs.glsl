6dd787172dd278c27b1335dc77ee06309ccd32d3
Alexey Kopytko <alexey@kopytko.com>
2026-05-18 08:01:17 +0900

    on-device counting
    | model                          |       size |     params | backend    | ngl | n_batch | n_ubatch | dev          |            test |                  t/s |
    | ------------------------------ | ---------: | ---------: | ---------- | --: | ------: | -------: | ------------ | --------------: | -------------------: |
    | olmoe A1.7B Q4_K - Medium      |   3.92 GiB |     6.92 B | Vulkan     | 999 |    4096 |     2048 | Vulkan0      |          pp2048 |     5524.58 ± 136.67 |
    | olmoe A1.7B Q4_K - Medium      |   3.92 GiB |     6.92 B | Vulkan     | 999 |    4096 |     2048 | Vulkan0      |           tg128 |        129.62 ± 1.06 |

--- ggml/src/ggml-vulkan/vulkan-shaders/mul_mm_id_funcs.glsl
+++ ggml/src/ggml-vulkan/vulkan-shaders/mul_mm_id_funcs.glsl
@@ -1,15 +1,13 @@
 #ifdef MUL_MAT_ID
-shared u16vec2 row_ids[BN];
+shared uvec2 row_ids[BN];
 uint _ne1;
 
 void load_row_ids(uint expert_idx, uint ic) {
-    _ne1 = data_expert_count[expert_idx];
-
-    const uint row_base = expert_idx * p.nei1 * p.nei0 + ic * BN;
+    const uint row_base = data_expert_offset[expert_idx];
+    _ne1 = data_expert_offset[expert_idx + 1] - row_base;
 
     for (uint i = gl_LocalInvocationIndex; i < BN && ic * BN + i < _ne1; i += BLOCK_SIZE) {
-        const uint row_id = data_row_ids[row_base + i];
-        row_ids[i] = u16vec2(row_id & 0xffff, row_id >> 16);
+        row_ids[i] = data_row_ids[row_base + ic * BN + i];
     }
     barrier();
 }
