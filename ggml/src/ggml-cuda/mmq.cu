c05495cb11d9734d0b6616a1271d3b72a8a78401
Alexey Kopytko <alexey@kopytko.com>
2026-05-22 16:00:58 +0900

    remove NVFP4 off MMQ onto the cuBLAS dequant

--- ggml/src/ggml-cuda/mmq.cu
+++ ggml/src/ggml-cuda/mmq.cu
@@ -269,13 +269,6 @@ bool ggml_cuda_should_use_mmq(enum ggml_type type, int cc, int64_t ne11, int64_t
     return false;
 #endif // GGML_CUDA_FORCE_CUBLAS
 
-    // MEASUREMENT (revert after): force NVFP4 off MMQ onto the cuBLAS dequant path (W4A16) to
-    // isolate weight-quant quality from the native FP4 W4A4 activation penalty.
-    static const bool nvfp4_force_dequant = getenv("GGML_NVFP4_FORCE_DEQUANT") != nullptr;
-    if (nvfp4_force_dequant && type == GGML_TYPE_NVFP4) {
-        return false;
-    }
-
     bool mmq_supported;
 
     switch (type) {
